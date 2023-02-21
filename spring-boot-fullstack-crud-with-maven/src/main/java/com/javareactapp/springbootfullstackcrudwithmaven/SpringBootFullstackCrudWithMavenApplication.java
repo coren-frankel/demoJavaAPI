f487d9c5321abc332192bfbca43ac0f3122e0a21
unclebabykern <coren.frankel@gmail.com>
2023-02-21 12:15:46 -0700

    Initial commit of Spring starter for Java API

---
+++ spring-boot-fullstack-crud-with-maven/src/main/java/com/javareactapp/springbootfullstackcrudwithmaven/SpringBootFullstackCrudWithMavenApplication.java
@@ -0,0 +1,13 @@
+package com.javareactapp.springbootfullstackcrudwithmaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFullstackCrudWithMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFullstackCrudWithMavenApplication.class, args);
+	}
+
+}
---
+++ spring-boot-fullstack-crud-with-maven/src/test/java/com/javareactapp/springbootfullstackcrudwithmaven/SpringBootFullstackCrudWithMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javareactapp.springbootfullstackcrudwithmaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFullstackCrudWithMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
